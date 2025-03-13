0844bfdecdcf03684676860d0ef4ae59a8ca070c
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-03-12 23:04:38 -0400

    basic page functionality

--- dummy.go
+++ dummy.go
@@ -1,15 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"time"
 )
 
+type PageVariables struct {
+	Date string
+	Time string
+}
+
 func indexPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(w, "Hello Gophers.")
+	now := time.Now()
+
+	HomePageVars := PageVariables{
+		Date: now.Format("02-01-2006"),
+		Time: now.Format("15:04:05"),
+	}
+
+	t, err := template.ParseFiles("home.html")
+	if err != nil {
+		log.Print("template parsing error: ", err)
+	}
+
+	err = t.Execute(w, HomePageVars)
+	if err != nil {
+		log.Print("template executing error: ", err)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", indexPage)
-	http.ListenAndServe(":8000", nil)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
