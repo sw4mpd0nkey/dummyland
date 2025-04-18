b7d5d788066cf282c94ba87c245e7d466b9edb78
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-04-18 00:33:18 -0400

    rework, working on sqli

--- dummy.go
+++ dummy.go
@@ -1,98 +1,64 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"log/slog"
 	"net/http"
-	"os"
-	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
-type Page struct {
-	Date  string
-	Time  string
-	Title string
-	Body  []byte
-}
-
-const TEMPLATES = "web/template/"
-
-func loadPage(pageToLoad string) (*Page, error) {
-
-	fileName := TEMPLATES + pageToLoad + ".html"
-	log.Println("+ opening file " + fileName)
-
-	body, err := os.ReadFile(fileName)
-	if err != nil {
-		return nil, err
-	}
-
-	log.Println("+ successfully found " + fileName)
-
-	return &Page{Title: pageToLoad, Body: body}, nil
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-
-	fileName := TEMPLATES + tmpl + ".html"
-	t, err := template.ParseFiles(fileName)
-
-	if err != nil {
-		log.Println("- error parsing file " + fileName)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = t.Execute(w, p)
-	if err != nil {
-		log.Println("- error rendering " + fileName)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	now := time.Now()
-
-	pageToLoad := r.URL.Path[len("/view/"):]
-	log.Println("+ loading title: " + pageToLoad)
-
-	page, err := loadPage(pageToLoad)
-	if err != nil {
-		// if not found log it and redirect to home
-		log.Println("- error loading page, redirecting home")
-		http.Redirect(w, r, "/", http.StatusFound)
-		return
-	} else {
-		page.Time = now.Format("13:13:45")
-		page.Date = now.Format("03/12/2025")
-	}
-
-	renderTemplate(w, pageToLoad, page)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-
-	now := time.Now()
-	pageToLoad := "home"
-
-	page, err := loadPage(pageToLoad)
-	if err != nil {
-		page = &Page{Title: pageToLoad}
-	} else {
-		page.Time = now.Format("13:13:45")
-		page.Date = now.Format("03/12/2025")
-	}
-
-	renderTemplate(w, pageToLoad, page)
-}
-
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-
+type File struct {
+	ID          uint
+	Filename    string
+	Signature   string
+	TenantID    string
+	DateCreated string
+	DateDeleted string
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/api/", apiHandler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	// Force log's color
+	gin.ForceConsoleColor()
+
+	router := gin.Default()
+
+	router.GET("/dummyland/image", func(c *gin.Context) {
+		// get tenantID from query parameter
+		tenantID := c.Query("tenantID")
+
+		db, err := sqlx.Open("sqlite3", "./dummy.db")
+		defer db.Close()
+
+		if err != nil {
+			slog.Error("Failed to open database", "error", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to open database"})
+			return
+		}
+
+		rows, err := db.Queryx("SELECT * FROM Files WHERE tenant_id = '" + tenantID + "'")
+		if err != nil {
+			slog.Error("Failed to query database", "error", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to query database"})
+			return
+		}
+		defer rows.Close()
+
+		var files []File
+		for rows.Next() {
+			var f File
+			err := rows.StructScan(&f)
+			if err != nil {
+				slog.Error("Failed to scan database row", "error", err)
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to scan database row"})
+				return
+			}
+			files = append(files, f)
+		}
+
+		// return files as an object in the JSON response
+		c.JSON(http.StatusOK, gin.H{"files": files})
+	})
+
+	router.Run() // listen and serve on 0.0.0.0:8080
 }
